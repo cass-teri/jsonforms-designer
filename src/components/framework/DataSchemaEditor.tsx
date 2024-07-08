d42dd32db9b0f7acbc435aa4ffc21396c9036753
Cassandra Thompson <cassandra.t.thompson@gov.ab.ca>
2024-07-08 16:35:11 -0600

    feature: swapping CodeMirror for Monaco editor for better code completion.

--- src/components/framework/DataSchemaEditor.tsx
+++ src/components/framework/DataSchemaEditor.tsx
@@ -1,11 +1,10 @@
 import React, { useContext } from "react"
-import CodeMirror from "@uiw/react-codemirror"
-import { json } from "@codemirror/lang-json"
 import {xcodeDark, xcodeLight} from "@uiw/codemirror-themes-all"
 import { useTheme } from "@/components/contexts/ThemeProvider.tsx"
 import { SchemaDesignerContext } from "@/components/contexts/SchemaContextProvider.tsx"
-import { vim } from "@replit/codemirror-vim"
 import { VimModeContext } from "@/components/contexts/VimModeContextProvider.tsx"
+import Editor from "@monaco-editor/react";
+import * as prettier from "prettier";
 
 export function DataSchemaEditor() {
     const { data_buffer, SetDataBuffer, SetIsDirty } = useContext(SchemaDesignerContext)
@@ -14,49 +13,43 @@ export function DataSchemaEditor() {
 
     const code_theme = theme.theme === "dark" ? xcodeDark : xcodeLight
 
-    function OnDrop(e: any) {
+    function OnDrop(e: any, data: any) {
         e.preventDefault()
         SetIsDirty(true)
         SetDataBuffer("")
     }
 
-    const onChange = React.useCallback(
-        //(val: string, viewUpdate: any) => {
-        (val: string) => {
-            SetIsDirty(true)
-            SetDataBuffer(val)
-        },
-        [SetIsDirty, SetDataBuffer]
-    )
+    async function OnChange(source: any,  event: any) {
+        SetIsDirty(true)
+        SetDataBuffer(source)
+    }
 
     //Clean this up, use push to extend the extensions array with vim instead of using a ternary
     return (
-        <div className="bg-background h-svh">
-            {vim_mode ? (
-                <CodeMirror
-                    theme={code_theme}
+        <div className="bg-background h-svh"
+            onDrop={OnDrop}
+        >
+
+                <Editor
                     value={data_buffer}
                     height="95vh"
                     width="100vw"
-                    extensions={[json(), vim()]}
-                    onChange={onChange}
-                    onDrop={OnDrop}
-                    basicSetup={{
-                        autocompletion: true,
-                        history: true
+                    defaultLanguage="json"
+                    options={{
+                        minimap: { enabled: false },
+                        wordWrap: "on",
+                        lineNumbers: "on",
+                        fontSize: 16,
+                        tabSize: 4,
+                        insertSpaces: true,
+                        autoIndent: "full",
+                        formatOnType: true,
+                        formatOnPaste: true,
+                        dragAndDrop: true,
+                        theme: theme.theme == "dark" ? "vs-dark": "vs-light",
                     }}
+                    onChange={OnChange}
                 />
-            ) : (
-                <CodeMirror
-                    theme={code_theme}
-                    value={data_buffer}
-                    height="95vh"
-                    width="100vw"
-                    extensions={[json()]}
-                    onChange={onChange}
-                    onDrop={OnDrop}
-                />
-            )}
         </div>
     )
 }
--- src/components/framework/UiSchemaEditor.tsx
+++ src/components/framework/UiSchemaEditor.tsx
@@ -1,96 +1,77 @@
-import React, { useContext } from "react"
-import CodeMirror from "@uiw/react-codemirror"
-import { json , jsonLanguage} from "@codemirror/lang-json"
-import { githubLight, githubDark } from "@uiw/codemirror-themes-all"
+import React, {useContext, useEffect} from "react"
 import { useTheme } from "@/components/contexts/ThemeProvider.tsx"
 import { SchemaDesignerContext } from "@/components/contexts/SchemaContextProvider.tsx"
-import { vim } from "@replit/codemirror-vim"
 import { VimModeContext } from "@/components/contexts/VimModeContextProvider.tsx"
-import {CompletionContext} from "@codemirror/autocomplete"
+import Editor  from "@monaco-editor/react"
+import * as prettier from "prettier"
+
 
 export function UiSchemaEditor() {
     const { ui_buffer, SetUiBuffer, SetIsDirty } = useContext(SchemaDesignerContext)
     const theme = useTheme()
     const { vim_mode } = useContext(VimModeContext)
 
-    function jsonCompletions(context: CompletionContext) {
-        const word = context.matchBefore(/\w*/)
-        if (word?.from == word?.to && !context.explicit)
-            return null
-        return {
-            from: word?.from,
-            options: [
-                {label: "match", type: "keyword"},
-                {label: "hello", type: "variable", info: "(World)"},
-                {label: "control", type: "text", apply: `
-               "_": {
-                    "type": "Control",
-                    "scope": "#/properties/_" 
-               }
-                `, detail: "macro"}
-            ]
-        }
-    }
+    const code_theme = theme.theme === "dark" ? "vs-dark" : "vs-light"
 
-    const jsonDocCompletion = jsonLanguage.data.of({
-        autocomplete:jsonCompletions
-    })
-
-    const code_theme = theme.theme === "dark" ?  githubDark: githubLight;
-
-    function OnDrop(e: any) {
+    function OnDrop(e: any, data: any) {
         e.preventDefault()
         SetIsDirty(true)
         SetUiBuffer("")
     }
 
-    const onChange = React.useCallback(
-        //(val: string, viewUpdate: any) => {
-        (val: string) => {
-            SetIsDirty(true)
-            SetUiBuffer(val)
-        },
-        [SetIsDirty, SetUiBuffer]
-    )
+    async function OnChange(source: any) {
+        SetIsDirty(true)
+        SetUiBuffer(source)
+    }
 
     //Clean this up, use push to extend the extensions array with vim instead of using a ternary
     return (
         <div className="bg-background h-svh">
             {vim_mode ? (
-                <CodeMirror
+                <Editor
                     theme={code_theme}
                     value={ui_buffer}
                     height="95vh"
                     width="100vw"
-                    basicSetup={{
-                        autocompletion: true,
-                        history: true,
-                        syntaxHighlighting: true,
-                        lineNumbers: true,
-                        completionKeymap: true,
+                   defaultLanguage="json"
+                    options={{
+                        minimap: { enabled: false },
+                        wordWrap: "on",
+                        lineNumbers: "on",
+                        fontSize: 16,
+                        tabSize: 4,
+                        insertSpaces: true,
+                        autoIndent: "full",
+                        formatOnType: true,
+                        formatOnPaste: true,
+                        dragAndDrop: true,
+                        theme: theme.theme == "dark" ? "vs-dark": "vs-light",
 
                     }}
-                    extensions={[json(), vim(), jsonDocCompletion]}
-                    onChange={onChange}
-                    onDrop={OnDrop}
+                    onChange={OnChange}
+
 
                 />
             ) : (
-                <CodeMirror
+                <Editor
                     theme={code_theme}
                     value={ui_buffer}
                     height="95vh"
                     width="100vw"
-                    basicSetup={{
-                        autocompletion: true,
-                        history: true,
-                        syntaxHighlighting: true,
-                        lineNumbers: true,
-                        completionKeymap: true
+                    options={{
+                        minimap: { enabled: false },
+                        wordWrap: "on",
+                        lineNumbers: "on",
+                        fontSize: 16,
+                        tabSize: 4,
+                        insertSpaces: true,
+                        autoIndent: "full",
+                        formatOnType: true,
+                        formatOnPaste: true,
+                        dragAndDrop: true,
+                        theme: theme.theme == "dark" ? "vs-dark": "vs-light",
                     }}
-                    extensions={[json(), jsonDocCompletion]}
-                    onChange={onChange}
-                    onDrop={OnDrop}
+                    onChange={OnChange}
                 />
             )}
         </div>
